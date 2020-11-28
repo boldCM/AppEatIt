439240ca2a733fd5be61d760a9f479e8d98a13e4
boldCM <carolin@cc-meier.de>
2020-11-28 19:00:35 +0100

    Create weekly ListItem

---
+++ client/src/components/WeekListItem/WeekListItem.js
@@ -0,0 +1,56 @@
+import React from "react";
+import styled from "styled-components/macro";
+import DiamondImg from "../../assets/diamond.png";
+import DiamondActiveImg from "../../assets/diamondActive.png";
+
+const ContainerWeekLI = styled.div`
+  display: flex;
+  align-items: center;
+`;
+
+const Diamond = styled.div`
+  background-image: url(${DiamondActiveImg});
+  height: 5.8rem;
+  width: 5.8rem;
+  background-repeat: no-repeat;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  color: var(--active-color);
+`;
+
+const DiamondLine = styled.div`
+  border-top: 1.5px solid var(--active-color);
+  width: 100px;
+  margin-left: -2px;
+  margin-bottom: 1px;
+`;
+
+const TextWeekLI = styled.div`
+  color: var(--active-color);
+  margin-bottom: 2px;
+  margin-left: 5px;
+`;
+
+const WeekListItem = () => {
+  return (
+    <ContainerWeekLI>
+      <Diamond>
+        {/* <img src={DiamondActiveImg} alt="Active Diamond" /> */}
+        28.11.
+      </Diamond>
+      <DiamondLine />
+      <TextWeekLI>Geschnetzeltes</TextWeekLI>
+    </ContainerWeekLI>
+  );
+};
+
+export default WeekListItem;
+
+// ein Eltern Div:
+
+// ein Div für diamond bild damit man den Text schön zentrieren kann
+// evtl ein weiteres Div für das Datum
+
+// ein Div für den Strich(oder bild als Strich)
+// ein Div für den TextRezept
---
+++ client/src/components/WeekListItem/WeekListItem.stories.js
@@ -0,0 +1,9 @@
+import React from "react";
+import WeekListItem from "./WeekListItem";
+
+export default {
+  title: "Example/Components/WeekListItem",
+  component: WeekListItem,
+};
+
+export const weekListItem = (args) => <WeekListItem {...args} />;
