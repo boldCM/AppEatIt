5d795d32112bcb8780f7241f6fd1c95f74ed014f
boldCM <carolin@cc-meier.de>
2020-11-30 14:49:56 +0100

    Fix scrolling Space in weeklyPage

--- client/src/pages/WeeklyPage/WeeklyPage.js
+++ client/src/pages/WeeklyPage/WeeklyPage.js
@@ -1,31 +1,38 @@
 import React from "react";
 import BottomNav from "../../components/BottomNav/BottomNav";
 // import PropTypes from "prop-types";
-
+import styled from "styled-components/macro";
 import Header from "../../components/Header";
 import WeekListItem from "../../components/WeekListItem/WeekListItem";
+
+const WeekListContainer = styled.div`
+  height: 26rem;
+  overflow: auto;
+`;
 
 const WeeklyPage = () => {
   return (
     <>
       <Header />
-      <WeekListItem />
-      <WeekListItem />
-      <WeekListItem />
-      <WeekListItem />
-      <WeekListItem />
-      <WeekListItem />
-      <WeekListItem />
-      <WeekListItem />
-      <WeekListItem />
-      <WeekListItem />
-      <WeekListItem />
-      <WeekListItem />
-      <WeekListItem />
-      <WeekListItem />
-      <WeekListItem />
-      <WeekListItem />
-      <WeekListItem />
+      <WeekListContainer>
+        <WeekListItem />
+        <WeekListItem />
+        <WeekListItem />
+        <WeekListItem />
+        <WeekListItem />
+        <WeekListItem />
+        <WeekListItem />
+        <WeekListItem />
+        <WeekListItem />
+        <WeekListItem />
+        <WeekListItem />
+        <WeekListItem />
+        <WeekListItem />
+        <WeekListItem />
+        <WeekListItem />
+        <WeekListItem />
+        <WeekListItem />
+      </WeekListContainer>
       <BottomNav />
     </>
   );
