2fbbe79b63b068635ca6388f68b46c406636673e
boldCM <carolin@cc-meier.de>
2020-12-02 18:57:52 +0100

    Create Recipe List Component

---
+++ client/src/components/RecipeList/RecipeList.js
@@ -0,0 +1,52 @@
+import React from "react";
+import { DiamondLine } from "../StyledLines/Lines";
+import styled from "styled-components/macro";
+import IconButton from "../BottomNav/IconButton";
+import CalendarCheck from "../../assets/calendarCheck.png";
+import CalendarFilled from "../../assets/calendarFilled.png";
+import CalendarFilledSVG from "../../assets/calendarFilled.svg";
+
+const ListContainer = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  text-align: center;
+  hr {
+    margin: 0;
+  }
+`;
+
+const RecipeItem = styled.div`
+  display: flex;
+  a {
+    margin: 1rem 0 0.8rem 0;
+  }
+  button {
+    padding: 0;
+    margin: 6px 0 0 8px;
+  }
+`;
+
+const RecipeList = () => {
+  return (
+    <>
+      <ListContainer>
+        <RecipeItem>
+          <a href="/">Lasagne</a>
+          <IconButton
+            iconSrc={CalendarFilledSVG}
+            iconAlt={"Item is in Calendar"}
+          />
+        </RecipeItem>
+        <DiamondLine />
+        <RecipeItem>
+          <a href="/">Geschnetzltes</a>
+          <IconButton iconSrc={CalendarCheck} iconAlt={"item in Calendar?"} />
+        </RecipeItem>
+        <DiamondLine />
+      </ListContainer>
+    </>
+  );
+};
+
+export default RecipeList;
