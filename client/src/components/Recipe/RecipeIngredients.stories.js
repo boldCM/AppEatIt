7c5d8c93c38855434725552835ec7d79f6357c59
boldCM <carolin@cc-meier.de>
2020-12-02 14:02:46 +0100

    Create RecipeIngredients

---
+++ client/src/components/Recipe/RecipeIngredients.stories.js
@@ -0,0 +1,10 @@
+import React from "react";
+
+import RecipeIngredients from "./RecipeIngredients";
+
+export default {
+  title: "Components",
+  component: RecipeIngredients,
+};
+
+export const recipeIngredients = (args) => <RecipeIngredients {...args} />;
