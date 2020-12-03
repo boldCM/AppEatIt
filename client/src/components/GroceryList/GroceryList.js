dc1110047eed5a554f0aed2077d062300203e9f3
boldCM <carolin@cc-meier.de>
2020-12-03 17:16:10 +0100

    Refractor GroceryListItem

--- client/src/components/GroceryList/GroceryList.js
+++ client/src/components/GroceryList/GroceryList.js
@@ -1,107 +1,58 @@
 import React from "react";
 import styled from "styled-components/macro";
-import IconButton from "../BottomNav/IconButton";
-import Checkbox from "../../assets/checkbox.svg";
-import CheckboxChecked from "../../assets/checkboxChecked.svg";
+import GroceryListItem from "./GroceryListItem";
 
 const ContainerGroceries = styled.div`
   display: flex;
   flex-direction: column;
-  margin-left: 20px;
-`;
-
-const ListItem = styled.p`
-  display: inline-flex;
-  margin: 0;
+  margin-left: 10px;
 `;
 
 const GroceryList = () => {
   return (
     <ContainerGroceries>
-      <ListItem>
-        <IconButton iconSrc={Checkbox} iconAlt="Checkbox" />
-        Kaffee
-      </ListItem>
-      <ListItem>
-        <IconButton iconSrc={CheckboxChecked} iconAlt="Checkbox" />
-        Klopapier
-      </ListItem>
-      <ListItem>
-        <IconButton iconSrc={CheckboxChecked} iconAlt="Checkbox" />
-        Klopapier
-      </ListItem>
-      <ListItem>
-        <IconButton iconSrc={CheckboxChecked} iconAlt="Checkbox" />
-        Klopapier
-      </ListItem>
-      <ListItem>
-        <IconButton iconSrc={CheckboxChecked} iconAlt="Checkbox" />
-        Klopapier
-      </ListItem>
-      <ListItem>
-        <IconButton iconSrc={CheckboxChecked} iconAlt="Checkbox" />
-        Klopapier
-      </ListItem>
-      <ListItem>
-        <IconButton iconSrc={CheckboxChecked} iconAlt="Checkbox" />
-        Klopapier
-      </ListItem>
-      <ListItem>
-        <IconButton iconSrc={CheckboxChecked} iconAlt="Checkbox" />
-        Klopapier
-      </ListItem>
-      <ListItem>
-        <IconButton iconSrc={CheckboxChecked} iconAlt="Checkbox" />
-        Klopapier
-      </ListItem>
-      <ListItem>
-        <IconButton iconSrc={CheckboxChecked} iconAlt="Checkbox" />
-        Klopapier
-      </ListItem>
-      <ListItem>
-        <IconButton iconSrc={CheckboxChecked} iconAlt="Checkbox" />
-        Klopapier
-      </ListItem>
-      <ListItem>
-        <IconButton iconSrc={CheckboxChecked} iconAlt="Checkbox" />
-        Klopapier
-      </ListItem>
-      <ListItem>
-        <IconButton iconSrc={CheckboxChecked} iconAlt="Checkbox" />
-        Klopapier
-      </ListItem>
-      <ListItem>
-        <IconButton iconSrc={CheckboxChecked} iconAlt="Checkbox" />
-        Klopapier
-      </ListItem>
-      <ListItem>
-        <IconButton iconSrc={CheckboxChecked} iconAlt="Checkbox" />
-        Klopapier
-      </ListItem>
-      <ListItem>
-        <IconButton iconSrc={CheckboxChecked} iconAlt="Checkbox" />
-        Klopapier
-      </ListItem>
-      <ListItem>
-        <IconButton iconSrc={CheckboxChecked} iconAlt="Checkbox" />
-        Klopapier
-      </ListItem>
-      <ListItem>
-        <IconButton iconSrc={CheckboxChecked} iconAlt="Checkbox" />
-        Klopapier
-      </ListItem>
-      <ListItem>
-        <IconButton iconSrc={CheckboxChecked} iconAlt="Checkbox" />
-        Klopapier
-      </ListItem>
-      <ListItem>
-        <IconButton iconSrc={CheckboxChecked} iconAlt="Checkbox" />
-        Klopapier
-      </ListItem>
-      <ListItem>
-        <IconButton iconSrc={CheckboxChecked} iconAlt="Checkbox" />
-        Klopapier
-      </ListItem>
+      <GroceryListItem Grocery="Olivenöl" />
+      <GroceryListItem Grocery="Olivenöl" />
+      <GroceryListItem Grocery="Olivenöl" />
+      <GroceryListItem Grocery="Olivenöl" />
+      <GroceryListItem Grocery="Olivenöl" />
+      <GroceryListItem Grocery="Olivenöl" />
+      <GroceryListItem Grocery="Olivenöl" />
+      <GroceryListItem Grocery="Olivenöl" />
+      <GroceryListItem Grocery="Olivenöl" />
+      <GroceryListItem Grocery="Olivenöl" />
+      <GroceryListItem Grocery="Olivenöl" />
+      <GroceryListItem Grocery="Olivenöl" />
+      <GroceryListItem Grocery="Olivenöl" />
+      <GroceryListItem Grocery="Olivenöl" />
+      <GroceryListItem Grocery="Olivenöl" />
+      <GroceryListItem Grocery="Olivenöl" />
+      <GroceryListItem Grocery="Olivenöl" />
+      <GroceryListItem Grocery="Olivenöl" />
+      <GroceryListItem Grocery="Olivenöl" />
+      <GroceryListItem Grocery="Olivenöl" />
+      <GroceryListItem Grocery="Olivenöl" />
+      <GroceryListItem Grocery="Olivenöl" />
+      <GroceryListItem Grocery="Olivenöl" />
+      <GroceryListItem Grocery="Olivenöl" />
+      <GroceryListItem Grocery="Olivenöl" />
+      <GroceryListItem Grocery="Olivenöl" />
+      <GroceryListItem Grocery="Olivenöl" />
+      <GroceryListItem Grocery="Olivenöl" />
+      <GroceryListItem Grocery="Olivenöl" />
+      <GroceryListItem Grocery="Olivenöl" />
+      <GroceryListItem Grocery="Olivenöl" />
+      <GroceryListItem Grocery="Olivenöl" />
+      <GroceryListItem Grocery="Olivenöl" />
+      <GroceryListItem Grocery="Olivenöl" />
+      <GroceryListItem Grocery="Olivenöl" />
+      <GroceryListItem Grocery="Olivenöl" />
+      <GroceryListItem Grocery="Olivenöl" />
+      <GroceryListItem Grocery="Olivenöl" />
+      <GroceryListItem Grocery="Olivenöl" />
+      <GroceryListItem Grocery="Olivenöl" />
+      <GroceryListItem Grocery="Olivenöl" />
+      <GroceryListItem Grocery="Olivenöl" />
     </ContainerGroceries>
   );
 };
--- client/src/components/GroceryList/GroceryListItem.js
+++ client/src/components/GroceryList/GroceryListItem.js
@@ -1,21 +1,37 @@
-import React from "react";
+import React, { useState } from "react";
 import styled from "styled-components/macro";
 import IconButton from "../BottomNav/IconButton";
 import Checkbox from "../../assets/checkbox.svg";
 import CheckboxChecked from "../../assets/checkboxChecked.svg";
+import PropTypes from "prop-types";
 
 const ListItem = styled.p`
   display: inline-flex;
-  margin: 0;
+  margin: 0.4rem 0;
+  button {
+    margin: 1px 5px 0 0;
+  }
 `;
 
-const GroceryListItem = () => {
+const GroceryListItem = ({ Grocery }) => {
+  const [ischecked, setIsChecked] = useState(false);
+
+  const CheckboxSrc = ischecked ? Checkbox : CheckboxChecked;
+
   return (
     <ListItem>
-      <IconButton iconSrc={Checkbox} iconAlt="Checkbox" />
-      Kaffee
+      <IconButton
+        iconSrc={CheckboxSrc}
+        iconAlt="Checkbox"
+        onClick={() => setIsChecked(!ischecked)}
+      />
+      {Grocery}
     </ListItem>
   );
+};
+
+GroceryListItem.propTypes = {
+  Grocery: PropTypes.string,
 };
 
 export default GroceryListItem;
