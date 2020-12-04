d3a37d7b374020d50fe735753ba36a6500928ca7
boldCM <carolin@cc-meier.de>
2020-12-04 13:53:25 +0100

    Add DatePicker and customize colors

--- client/src/components/WeekListItem/WeekListItem.js
+++ client/src/components/WeekListItem/WeekListItem.js
@@ -1,7 +1,30 @@
-import React from "react";
+import React, { useState } from "react";
 import styled from "styled-components/macro";
 import DiamondImg from "../../assets/diamond.svg";
 import { DiamondLine } from "../styledComponents/Lines";
+import DatePicker from "react-datepicker";
+import "react-datepicker/dist/react-datepicker.css";
+// import "react-datepicker/src/stylesheets/datepicker.scss";
+// import "node_modules/react-datepicker/src/stylesheets/datepicker-cssmodules.scss";
+// client/node_modules/react-datepicker/src/stylesheets/datepicker.scss
+
+// const StyledDatePicker = styled(DatePicker)`
+//   background-color: pink;
+//   border-radius: 10px;
+//   border: none;
+//    {
+//     background-color: pink;
+//   }
+
+//   .react-datepicker__header {
+//     text-align: center;
+//     background-color: pink;
+//     border-bottom: 1px solid $datepicker__border-color;
+//     border-top-left-radius: $datepicker__border-radius;
+//     padding-top: 8px;
+//     position: relative;
+//   }
+// `;
 
 const ContainerWeek = styled.div`
   display: flex;
@@ -29,6 +52,7 @@ const TextWeek = styled.a`
 `;
 
 const WeekListItem = () => {
+  const [date, setDate] = useState(new Date());
   return (
     <ContainerWeek>
       <Diamond>
@@ -39,9 +63,26 @@ const WeekListItem = () => {
         </time>
       </Diamond>
       <DiamondLine />
+      {/* <label>
+        <input type="date" />
+      </label> */}
+      <DatePicker
+        selected={date}
+        onChange={(selectedDate) => setDate(selectedDate)}
+        dateFormat="d.MM."
+      />
       <TextWeek href="/">Geschnetzeltes</TextWeek>
     </ContainerWeek>
   );
 };
 
 export { WeekListItem };
+
+// holt mir das aktuelle Datum+Uhrzeit:
+const day = new Date();
+// Array mit den Wochentagen:
+const weekday = ["So", "Mo", "Di", "Mi", "Do", "Fr", "Sa"];
+// Schreibt mir den aktuellen Wochentag, indem er ihn aus dem array zieht.
+// getDay arbeitet mit dem Index des Arrays, wobei 0 der Sonntag ist
+// dh mit day.getDay() lege ich die Stelle im Array fest, die ich ausgegeben haben will.
+document.write("Today is " + weekday[day.getDay()]);
