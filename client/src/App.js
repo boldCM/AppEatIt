38a0e96846ba568b50d7209a7a3e538f9043179a
boldCM <carolin@cc-meier.de>
2020-12-05 16:33:28 +0100

    Clean up App.js

--- client/src/App.js
+++ client/src/App.js
@@ -1,6 +1,6 @@
 import React from "react";
 import GlobalStyle from "./GlobalStyle";
-import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 import WeeklyPage from "./pages/WeeklyPage/WeeklyPage";
 import SingleRecipe from "./pages/SingleRecipePage/SingleRecipePage";
 import RecipeListPage from "./pages/RecipeListPage/RecipeListPage";
