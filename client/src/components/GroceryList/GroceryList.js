80113c594856115e6b6e3ebf85e1d9946d5b1821
boldCM <carolin@cc-meier.de>
2020-12-17 13:43:44 +0100

    Get Grocerylist from week

--- client/src/components/GroceryList/GroceryList.js
+++ client/src/components/GroceryList/GroceryList.js
@@ -9,8 +9,6 @@ const ContainerGroceries = styled.div`
   margin-left: 10px;
 `;
 
-// 1. Get Groceries
-// 2. Map each Groceries
 // 3. filtern nach doubled Groceries
 // 4. addiere doubled Groceries
 // dabei noch den String zur Number convertieren, durch +(Zahl) oder durch Number(Zahl)
@@ -19,62 +17,33 @@ const ContainerGroceries = styled.div`
 // vllt noch in Klammern zu welchem Rezept die Groceries gehören? (also RecipeName mitgeben)
 
 const GroceryList = () => {
-  // um später über jedes einzelne Grocerie zu mappen möchte ich den State nutzen
   // const [groceries, setGroceries] = useState([]);
-  // ersmal das Object speichern:
   const [recipeObject, setRecipeObject] = useState([]);
 
-  // getGroceries from week:
   useEffect(() => {
     async function getRecipesInWeek() {
       const getRecipeObjects = await getWeek();
+      console.log(getRecipeObjects);
       setRecipeObject(getRecipeObjects);
     }
     getRecipesInWeek();
   }, []);
 
-  // gibt mir beide Recipe-Objekte:
-  const filterRecipe = recipeObject.filter((item) => item.Recipe);
-  console.log(filterRecipe);
+  console.log(recipeObject);
 
-  // das ertse Objekt allein, das matched:
-  const findIngredients = filterRecipe.find((item) => item.Recipe.Ingredients);
-  console.log(findIngredients);
+  const groceryArrays = recipeObject?.map((ingredient) => {
+    return ingredient.Recipe.Ingredients;
+  });
 
-  // destructuring funktioniert:
-  // const { RecipeName } = findIngredients;
-  // console.log(RecipeName);
-  // const doubleArray = filterRecipe.Recipe;
-  // console.log(doubleArray);
+  const oneGroceryArray = [].concat.apply([], groceryArrays);
 
-  const Array = findIngredients.Recipe.Ingredients;
-  console.log(Array);
-
-  // Für jedes Objekt in filterRecipe will ich die Ingredients darausgelesen haben.
-  // also für jedes Object einmal Array (Z.50) machen und dann unten mappen
-  // let arrayLength = recipeObject.length;
-
-  // const Dings = (filterRecipe) => {
-  //   for (let i = 0; i <= arrayLength; i++) {
-  //     const Array = filterRecipe.Recipe.Ingredients;
-  //     return Array?.map((ingredient) => (
-  //       <GroceryListItem key={ingredient} Grocery={ingredient} />
-  //     ));
-  //   }
-  // };
-
-  // gibt mir zwei undefined Arrays
-  // const anotherArray = filterRecipe.map((recipe) => recipe.findIngredients);
-  // console.log(anotherArray);
-
-  // console.log(recipeObject.length);
+  console.log(oneGroceryArray);
 
   return (
     <ContainerGroceries>
-      {Array?.map((ingredient) => (
+      {oneGroceryArray?.map((ingredient) => (
         <GroceryListItem key={ingredient} Grocery={ingredient} />
       ))}
-      {/* {Dings()} */}
     </ContainerGroceries>
   );
 };
