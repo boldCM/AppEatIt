298ff39a19ca3d1993406be628e561d15827c870
boldCM <carolin@cc-meier.de>
2020-12-18 11:39:10 +0100

    List nested object after restructuring Data in MongoDB

--- client/src/components/GroceryList/GroceryList.js
+++ client/src/components/GroceryList/GroceryList.js
@@ -18,6 +18,9 @@ const ContainerGroceries = styled.div`
 
 const GroceryList = () => {
   const [recipeObject, setRecipeObject] = useState([]);
+  const [grocery, setGrocery] = useState("");
+  const [quantity, setQuantity] = useState("");
+  const [unit, setUnit] = useState("");
 
   useEffect(() => {
     async function getRecipesInWeek() {
@@ -27,14 +30,70 @@ const GroceryList = () => {
     getRecipesInWeek();
   }, []);
 
-  const oneGroceryArray = recipeObject.flatMap((ingredient) => {
+  const oneShoppingListArray = recipeObject.flatMap((ingredient) => {
     return ingredient.Recipe.Ingredients;
   });
+  console.log(oneShoppingListArray);
+
+  // const iterator1 = oneShoppingListArray.entries();
+
+  // console.log(iterator1.next().value);
+
+  // const iteratedArray= oneShoppingListArray.map((object)=>{
+  // const bla = (oneShoppingListArray) => {
+  // for (let i = 0; i < oneShoppingListArray.length; i++) {
+  //   const element = oneShoppingListArray[i];
+  //   for (const [key, value] of Object.entries(element)) {
+  //     console.log(`${key}: ${value}`);
+  //     return `${key}: ${value}`;
+  //   }
+  // }
+  // console.log(bla(oneShoppingListArray));
+  // ich will von jeder Stelle im Array, also von jedem Objekt erstmal das Value vom Grocery-Key haben.
+  // console.log(oneShoppingListArray[0]);
+  // const { Grocery, Unit, Quantity } = oneShoppingListArray[0];
+  // console.log(Grocery);
+  // console.log(Unit);
+  // console.log(Quantity);
+  // wenn ich für jede Stelle im Array destructere und dann rendere?
+  // kann ich für jede Stelle einen useState einsetzen?
+  // das heißt oneShoppingListArray mappen...?
+
+  // const iteratedArray = oneShoppingListArray.map((object) => {
+  //   const { Grocery, Unit, Quantity } = object;
+  //   setUnit(Unit);
+  //   setGrocery(Grocery);
+  //   setQuantity(Quantity);
+  //   return Grocery;
+  // });
+  // console.log(iteratedArray);
+
+  // Object.is() vergleich Objekte!!!
+
+  // console.log(iteratedArray)
+
+  // zahlen parsen
+
+  // console.log(parseInt(oneShoppingListArray));
+
+  // console.log(oneShoppingListArray.valueOf());
+
+  // gibt mir alle Zahlen wieder:
+  // const Numbers = oneShoppingListArray?.map((ingredient) => parseInt(ingredient));
+
+  // und den rest slicen, stellen im array gleich lassen und addieren?-->aber dann ist die Stelle vom alten Mehl gekürzt
 
   return (
     <ContainerGroceries>
-      {oneGroceryArray?.map((ingredient) => (
+      {/* {oneShoppingListArray?.map((ingredient) => (
         <GroceryListItem key={ingredient} Grocery={ingredient} />
+      ))} */}
+
+      {oneShoppingListArray?.map((object) => (
+        <GroceryListItem
+          key={object.Grocery}
+          Grocery={object.Quantity + " " + object.Unit + " " + object.Grocery}
+        />
       ))}
     </ContainerGroceries>
   );
