608bbb7c7f568af3943a68e2de2702828d62269b
boldCM <carolin@cc-meier.de>
2020-12-21 17:50:33 +0100

    Implement Splash Page in Router

--- client/src/App.js
+++ client/src/App.js
@@ -1,12 +1,19 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import GlobalStyle from "./GlobalStyle";
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 import WeeklyPage from "./pages/WeeklyPage/WeeklyPage";
 import SingleRecipe from "./pages/SingleRecipePage/SingleRecipePage";
 import RecipeListPage from "./pages/RecipeListPage/RecipeListPage";
 import GroceryListPage from "./pages/GroceryListPage/GroceryListPage";
+import Splashpage from "./pages/SplashPage/SplashPage";
 
 function App() {
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    setTimeout(() => setLoading(false), 4100);
+  });
+
   return (
     <>
       <Router>
@@ -21,9 +28,7 @@ function App() {
           <Route path="/:RecipeName">
             <SingleRecipe />
           </Route>
-          <Route path="/">
-            <WeeklyPage />
-          </Route>
+          <Route path="/">{loading ? <Splashpage /> : <WeeklyPage />}</Route>
         </Switch>
       </Router>
     </>
