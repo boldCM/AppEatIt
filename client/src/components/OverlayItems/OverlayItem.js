25013a3f9eb4a9837bfdfb8eadaecf29f8d3dad3
boldCM <carolin@cc-meier.de>
2020-12-29 20:07:27 +0100

    Remove unused vars

--- client/src/components/OverlayItems/OverlayItem.js
+++ client/src/components/OverlayItems/OverlayItem.js
@@ -1,5 +1,5 @@
-import React, { useEffect } from "react";
-import { Link, Redirect, useHistory, useParams } from "react-router-dom";
+import React from "react";
+import { Link, useHistory, useParams } from "react-router-dom";
 import styled from "styled-components/macro";
 import { OverlayLine, UnderlineHeaderOverlay } from "../styledComponents/Lines";
 import IconButton from "../BottomNav/IconButton";
